3f13f2e10491302dd04bcd1ae654877d2ad26c01
syruphanabi <469218045@qq.com>
2019-04-12 00:21:04 -0400

    -

--- givensqr.m
+++ givensqr.m
@@ -1,21 +1,37 @@
 function [Q, R] = givensqr(A)
+
 [m,n] = size(A);
 Q = eye(m);
 for i = 1:n
-    for j = i+1:m 
-        [c,s] = givens(A(i,i), A(j,i));
-        G = [c,s;-s,c]';
-        B = G*[A(i,:); A(j,:)];
-        A(i,:) = B(1,:);
-        A(j,:) = B(2,:);
-        G_full = eye(m);
-        G_full(i,i) = c;
-        G_full(j,i) = -s;
-        G_full(i,j) = s;
-        G_full(j,j) = c;
-        Q = G_full'*Q;
+    for j = i+1:m
+%         [c,s] = givens(A(i,i), A(j,i));
+        
+%         G = [c,s;-s,c]';
+%         B = G*[A(i,:); A(j,:)];
+%         A(i,:) = B(1,:);
+%         A(j,:) = B(2,:);
+        a = A(i,i); b = A(j,i);
+        if abs(b) > abs(a)
+            if b == 0
+                c = 1; s = 0; return;
+            end
+            r = -a/b; s = 1/sqrt(1+r*r);c = s*r;
+        else
+            r = -b/a; c = 1/sqrt(1+r*r);s = c*r;
+        end
+
+        ti = A(i,:);
+        tj = A(j,:);
+        A(i,:) = c*ti - s*tj;
+        A(j,:) = s*ti + c*tj;
+        A(j,i) = 0;
+        
+        ti = Q(:,i);
+        tj = Q(:,j);
+        Q(:,i) = c*ti - s*tj;
+        Q(:,j) = s*ti + c*tj;
+     
     end
 end
 R = A;
-Q = Q';
 end
